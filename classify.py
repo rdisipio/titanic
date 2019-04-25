099d6fbde1e067595ef44208726e5629bf0f6723
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2019-04-25 08:21:48 -0400

    added xgboost

--- classify.py
+++ classify.py
@@ -13,6 +13,9 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
+
+from xgboost import XGBClassifier
 
 from data_preprocessing import *
 
@@ -49,41 +52,58 @@ X_test  = test_df[features]
 #   print( "test", f, X_test[f].isnull().sum(), np.isnan(X_test[f]).sum() )
 
 # Random Forest
-random_forest = RandomForestClassifier(n_estimators=200)
-random_forest.fit(X_train, Y_train)
-Y_prediction = random_forest.predict(X_test)
-random_forest.score(X_train, Y_train)
-acc_random_forest = round(random_forest.score(X_train, Y_train) * 100, 2)
+rf = RandomForestClassifier(n_estimators=200)
+rf.fit(X_train, Y_train)
+Y_pred_rf = rf.predict(X_test)
+rf.score(X_train, Y_train)
+acc_rf = round(rf.score(X_train, Y_train) * 100, 2)
 
 # Logistic regression
 logreg = LogisticRegression()
 logreg.fit(X_train, Y_train)
-Y_pred = logreg.predict(X_test)
+Y_pred_logreg = logreg.predict(X_test)
 acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
 
 # Naive Bayes
-gaussian = GaussianNB()
-gaussian.fit(X_train, Y_train)
-Y_pred = gaussian.predict(X_test)
-acc_gaussian = round(gaussian.score(X_train, Y_train) * 100, 2)
+bayes = GaussianNB()
+bayes.fit(X_train, Y_train)
+Y_pred_bayes = bayes.predict(X_test)
+acc_bayes = round(bayes.score(X_train, Y_train) * 100, 2)
+
+# XGBoost
+#objective = "binary:hinge"
+objective = "binary:logistic"
+bdt = XGBClassifier(objective=objective, max_depth=6)
+bdt.fit( X_train, Y_train )
+Y_pred_bdt = bdt.predict(X_test)
+acc_bdt = round( accuracy_score( bdt.predict(X_train), Y_train ) * 100, 2 )
 
 # Put results together
 results = pd.DataFrame({
-    'Model': [ 'RandomForest', 'LogisticRegression', 'NaiveBayes' ],
-    'Score': [ acc_random_forest, acc_log, acc_gaussian ]
+    'Model': [ 'RandomForest', 'LogisticRegression', 'NaiveBayes', 'BDT' ],
+    'Score': [ acc_rf, acc_log, acc_bayes, acc_bdt ]
     })
 
 result_df = results.sort_values(by='Score', ascending=False)
 result_df = result_df.set_index('Score')
 print(result_df.head(9))
 
-scores = cross_val_score(random_forest, X_train, Y_train, cv=10, scoring = "accuracy")
+scores = cross_val_score(rf, X_train, Y_train, cv=10, scoring = "accuracy")
 print("INFO: cross-validation")
 print("Scores:", scores)
 print("Mean:", scores.mean())
 print("Standard Deviation:", scores.std())
 
 print("INFO: features ranking")
-importances = pd.DataFrame({'feature':X_train.columns,'importance':np.round(random_forest.feature_importances_,3)})
+importances = pd.DataFrame({'feature':X_train.columns,'importance':np.round(rf.feature_importances_,3)})
 importances = importances.sort_values('importance',ascending=False).set_index('feature')
 print(importances.head(len(features)))
+
+print("INFO: preparing submission file:")
+fname = "data/submission.csv"
+df = pd.DataFrame(  [ test_df["PassengerId"], Y_pred_rf ] )
+df = df.transpose()
+df.columns = [ "PassengerId", "Survived"]
+df.set_index("PassengerId",inplace=True)
+print(df.head(10))
+df.to_csv( fname, header=True, index=True )
