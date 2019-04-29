cf344cfa368d86b29132fcc2ea32786ea9482b59
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2019-04-29 09:41:00 -0400

    missing age from group median

--- classify.py
+++ classify.py
@@ -12,8 +12,10 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
 
 from xgboost import XGBClassifier
 
@@ -25,29 +27,49 @@ train_df = pd.read_csv("data/train.csv")
 test_df = pd.read_csv("data/test.csv")
 
 # print out example
-print("INFO: training sample size:", len(train_df) )
-print("INFO: testing sample size:", len(test_df) )
+print("INFO: training sample size:", len(train_df))
+print("INFO: testing sample size:", len(test_df))
 
-for data in [ train_df, test_df ]:
-   process_family( data )
-   process_age(data)
-   process_cabin( data )
-   process_embarkment( data )
-   process_title(data )
-   process_fare(data)
-   process_gender(data)
+for data in [train_df, test_df]:
+    # order matters!
 
-print(train_df.describe() )
+    process_family(data)
+    process_cabin(data)
+    process_embarkment(data)
+    process_title(data)
+    process_fare(data)
+    process_gender(data)
+    process_age(data)
 
-features = [ 'Pclass', 'Sex', 'Embarked', 'FamilySize', 
-             'Deck', 'Title', 'Fare', 'FareBin', 'Age', 'AgeBin', ]
+print(train_df.describe())
+
+features = ['Pclass', 'Sex', 'Embarked',
+            'FamilySize', 'Parch', 'SibSp',
+            'Deck', 'Title',
+            'Fare', 'FareBin',
+            'Age', 'AgeBin', ]
 X_train = train_df[features]
 Y_train = train_df["Survived"]
-X_test  = test_df[features]
+X_test = test_df[features]
+
+# Apply standardization?
+#scaler = StandardScaler()
+#scaler = MinMaxScaler([-1, 1])
+#X_train = scaler.fit_transform(X_train)
+#X_test = scaler.transform(X_test)
+#X_train = pd.DataFrame(X_train)
+#X_test = pd.DataFrame(X_test)
+
+# Apply PCA?
+#pca = PCA(n_components=8)
+#pc_train = pca.fit_transform(X_train)
+#X_train = pd.DataFrame(data = pc_train)
+#pc_test = pca.transform(X_test)
+#X_test  = pd.DataFrame(data = pc_test)
 
 # Check invalid fields
-#print(features)
-#for f in features:
+# print(features)
+# for f in features:
 #   print( "train", f, X_train[f].isnull().sum(), np.isnan(X_train[f]).sum() )
 #   print( "test", f, X_test[f].isnull().sum(), np.isnan(X_test[f]).sum() )
 
@@ -74,37 +96,39 @@ acc_bayes = round(bayes.score(X_train, Y_train) * 100, 2)
 objective = "binary:hinge"
 #objective = "binary:logistic"
 bdt = XGBClassifier(objective=objective, max_depth=6)
-bdt.fit( X_train, Y_train )
+bdt.fit(X_train, Y_train)
 Y_pred_bdt = bdt.predict(X_test)
-acc_bdt = round( accuracy_score( bdt.predict(X_train), Y_train ) * 100, 2 )
+acc_bdt = round(accuracy_score(bdt.predict(X_train), Y_train) * 100, 2)
 
 # Put results together
 results = pd.DataFrame({
-    'Model': [ 'RandomForest', 'LogisticRegression', 'NaiveBayes', 'BDT' ],
-    'Score': [ acc_rf, acc_log, acc_bayes, acc_bdt ]
-    })
+    'Model': ['RandomForest', 'LogisticRegression', 'NaiveBayes', 'BDT'],
+    'Score': [acc_rf, acc_log, acc_bayes, acc_bdt]
+})
 
 result_df = results.sort_values(by='Score', ascending=False)
 result_df = result_df.set_index('Score')
 print(result_df.head(9))
 
-scores = cross_val_score(bdt, X_train, Y_train, cv=10, scoring = "accuracy")
+scores = cross_val_score(bdt, X_train, Y_train, cv=10, scoring="accuracy")
 print("INFO: cross-validation")
 print("Scores:", scores)
 print("Mean:", scores.mean())
 print("Standard Deviation:", scores.std())
 
 print("INFO: features ranking")
-importances = pd.DataFrame({'feature':X_train.columns,'importance':np.round(bdt.feature_importances_,3)})
-importances = importances.sort_values('importance',ascending=False).set_index('feature')
+importances = pd.DataFrame(
+    {'feature': X_train.columns, 'importance': np.round(bdt.feature_importances_, 3)})
+importances = importances.sort_values(
+    'importance', ascending=False).set_index('feature')
 print(importances.head(len(features)))
 
 print("INFO: preparing submission file:")
 fname = "data/submission.csv"
-#df = pd.DataFrame(  [ test_df["PassengerId"], Y_pred_rf ] )
-df = pd.DataFrame(  [ test_df["PassengerId"], Y_pred_bdt ] )
+df = pd.DataFrame([test_df["PassengerId"], Y_pred_rf])
+#df = pd.DataFrame([test_df["PassengerId"], Y_pred_bdt])
 df = df.transpose()
-df.columns = [ "PassengerId", "Survived"]
-df.set_index("PassengerId",inplace=True)
+df.columns = ["PassengerId", "Survived"]
+df.set_index("PassengerId", inplace=True)
 print(df.head(10))
-df.to_csv( fname, header=True, index=True )
+df.to_csv(fname, header=True, index=True)
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,100 +1,114 @@
 import numpy as np
 import pandas as pd
 
+
 def process_gender(df):
-   gender_dict = { "male": 0, "female":1 }
-   df['Sex'] = df['Sex'].map(gender_dict)
+    gender_dict = {"male": 0, "female": 1}
+    df['Sex'] = df['Sex'].map(gender_dict)
+
 
 def process_embarkment(df):
-   df['Embarked'].fillna('S', inplace=True )
+    df['Embarked'].fillna('S', inplace=True)
 
-   ports = {"S": 0, "C": 1, "Q": 2}
-   df['Embarked'] = df['Embarked'].map(ports)
+    ports = {"S": 0, "C": 1, "Q": 2}
+    df['Embarked'] = df['Embarked'].map(ports)
+
 
 def process_family(df):
-   df['FamilySize'] = df['Parch'] + df['SibSp'] + 1
+    df['FamilySize'] = df['Parch'] + df['SibSp'] + 1
 
-   df['FamilySizeBin'] = df['FamilySize']
-   df.loc[ df['FamilySizeBin'] == 1, 'FamilySizeBin' ] = 0
-   df.loc[ (df['FamilySizeBin']>=2) & (df['FamilySizeBin']<=4), 'FamilySizeBin' ] = 1
-   df.loc[ df['FamilySizeBin']>4, 'FamilySizeBin' ] = 2
+    df['FamilySizeBin'] = df['FamilySize']
+    df.loc[df['FamilySizeBin'] == 1, 'FamilySizeBin'] = 0
+    df.loc[(df['FamilySizeBin'] >= 2) & (
+        df['FamilySizeBin'] <= 4), 'FamilySizeBin'] = 1
+    df.loc[df['FamilySizeBin'] > 4, 'FamilySizeBin'] = 2
+
 
 def process_age(df):
-   mean = df["Age"].mean()
-   std = df["Age"].std()
-   is_null = df["Age"].isnull().sum()
-   # compute random numbers between the mean, std and is_null
-   rand_age = np.random.randint(mean - std, mean + std, size = is_null)
-   # fill NaN values in Age column with random values generated
-   age_slice = df["Age"].copy()
-   age_slice[np.isnan(age_slice)] = rand_age
-   df["Age"] = age_slice
-   df["Age"] = df["Age"].astype(int)
 
-   df['AgeBin'] = df['Age'].astype(int)
-   df.loc[ df['Age'] <= 11, 'AgeBin'] = 0
-   df.loc[(df['Age'] > 11) & (df['Age'] <= 18), 'AgeBin'] = 1
-   df.loc[(df['Age'] > 18) & (df['Age'] <= 25), 'AgeBin'] = 2
-   df.loc[(df['Age'] > 25) & (df['Age'] <= 35), 'AgeBin'] = 3
-   df.loc[(df['Age'] > 35) & (df['Age'] <= 45), 'AgeBin'] = 4
-   df.loc[(df['Age'] > 45) & (df['Age'] <= 60), 'AgeBin'] = 5
-   df.loc[ df['Age'] > 60, 'AgeBin'] = 6
+    # age has a non-uniform distribution, depends on sex, ticket class and title
+    groups = df.groupby(['Sex', 'Pclass', 'Title']).median()
+    df['Age'] = df.apply(lambda row: groups.loc[row.Sex, row.Pclass,
+                                                row.Title].Age if np.isnan(row.Age) else row.Age, axis=1)
+
+    #mean = df["Age"].mean()
+    #std = df["Age"].std()
+    #is_null = df["Age"].isnull().sum()
+    # compute random numbers between the mean, std and is_null
+    #rand_age = np.random.randint(mean - std, mean + std, size=is_null)
+    # fill NaN values in Age column with random values generated
+    #age_slice = df["Age"].copy()
+    #age_slice[np.isnan(age_slice)] = rand_age
+    #df["Age"] = age_slice
+    #df["Age"] = df["Age"].astype(int)
+
+    df['AgeBin'] = df['Age'].astype(int)
+    df.loc[df['Age'] <= 11, 'AgeBin'] = 0
+    df.loc[(df['Age'] > 11) & (df['Age'] <= 18), 'AgeBin'] = 1
+    df.loc[(df['Age'] > 18) & (df['Age'] <= 25), 'AgeBin'] = 2
+    df.loc[(df['Age'] > 25) & (df['Age'] <= 35), 'AgeBin'] = 3
+    df.loc[(df['Age'] > 35) & (df['Age'] <= 45), 'AgeBin'] = 4
+    df.loc[(df['Age'] > 45) & (df['Age'] <= 60), 'AgeBin'] = 5
+    df.loc[df['Age'] > 60, 'AgeBin'] = 6
+
 
 def process_fare(df):
     df['Fare'] = df['Fare'].fillna(0)
 #    df['Fare'] = df['Fare'].astype(int)
-    df.loc[ df['Fare'] <= 10, 'FareBin'] = 0
+    df.loc[df['Fare'] <= 10, 'FareBin'] = 0
     df.loc[(df['Fare'] > 10) & (df['Fare'] <= 20), 'FareBin'] = 1
-    df.loc[(df['Fare'] > 20) & (df['Fare'] <= 30), 'FareBin']   = 2
-    df.loc[(df['Fare'] > 30) & (df['Fare'] <= 50), 'FareBin']   = 3
-    df.loc[(df['Fare'] > 50) & (df['Fare'] <= 100), 'FareBin']   = 4
-    df.loc[(df['Fare'] > 100) & (df['Fare'] <= 200), 'FareBin']   = 5
-    df.loc[ df['Fare'] > 200, 'FareBin'] = 6
+    df.loc[(df['Fare'] > 20) & (df['Fare'] <= 30), 'FareBin'] = 2
+    df.loc[(df['Fare'] > 30) & (df['Fare'] <= 50), 'FareBin'] = 3
+    df.loc[(df['Fare'] > 50) & (df['Fare'] <= 100), 'FareBin'] = 4
+    df.loc[(df['Fare'] > 100) & (df['Fare'] <= 200), 'FareBin'] = 5
+    df.loc[df['Fare'] > 200, 'FareBin'] = 6
     df['FareBin'] = df['FareBin'].astype(int)
 
+
 def process_cabin(df):
-#   import re
-   deck = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "U": 8}
-   df['Cabin'].fillna("U0", inplace=True)
-   df['Deck'] = df['Cabin'].map(lambda x: x[0])
+    #   import re
+    deck = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "U": 8}
+    df['Cabin'].fillna("U0", inplace=True)
+    df['Deck'] = df['Cabin'].map(lambda x: x[0])
 #   df['Deck'] = df['Cabin'].map(lambda x: re.compile("([a-zA-Z]+)").search(x).group())
-   df['Deck'] = df['Deck'].map(deck)
-   df['Deck'] = df['Deck'].fillna(0)
-   df['Deck'] = df['Deck'].astype(int)
+    df['Deck'] = df['Deck'].map(deck)
+    df['Deck'] = df['Deck'].fillna(0)
+    df['Deck'] = df['Deck'].astype(int)
 
 
 def process_title(df):
 
-   # 0=officer, 1=Royalty, 2=commoner(male), 3=commoner(female)
-   titles_dict = {
-      "officer" : 0,
-      "royalty" : 1,
-      "man"     : 2,
-      "woman"   : 3
-   }
+    # 0=officer, 1=Royalty, 2=commoner(male), 3=commoner(female)
+    titles_dict = {
+        "officer": 0,
+        "royalty": 1,
+        "man": 2,
+        "woman": 3
+    }
 
-   title_dict = {
-     "Capt": 0,
-     "Col": 0,
-     "Major": 0,
-     "Dr": 2,
-     "Rev": 2,
-     "Jonkheer": 1,
-     "Don": 2,
-     "Sir" : 1,
-     "the Countess": 1,
-     "Countess" : 1,
-     "Lady" : 1,
-     "Mme": 3,
-     "Mlle": 3,
-     "Ms": 3,
-     "Mr" : 2,
-     "Mrs" : 3,
-     "Miss" : 3,
-     "Master" : 2,
-   }
+    title_dict = {
+        "Capt": 0,
+        "Col": 0,
+        "Major": 0,
+        "Dr": 2,
+        "Rev": 2,
+        "Jonkheer": 1,
+        "Don": 2,
+        "Sir": 1,
+        "the Countess": 1,
+        "Countess": 1,
+        "Lady": 1,
+        "Mme": 3,
+        "Mlle": 3,
+        "Ms": 3,
+        "Mr": 2,
+        "Mrs": 3,
+        "Miss": 3,
+        "Master": 2,
+    }
 
-   #18,1,2,"Williams, Mr. Charles Eugene",male,,0,0,244373,13,,S
-   df['Title'] = df['Name'].map(lambda name:name.split(',')[1].split('.')[0].strip())
-   df['Title'] = df.Title.map(title_dict)
-   df['Title'] = df['Title'].fillna(2)
+    # 18,1,2,"Williams, Mr. Charles Eugene",male,,0,0,244373,13,,S
+    df['Title'] = df['Name'].map(lambda name: name.split(',')[
+                                 1].split('.')[0].strip())
+    df['Title'] = df.Title.map(title_dict)
+    df['Title'] = df['Title'].fillna(2)
